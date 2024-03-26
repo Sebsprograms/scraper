e48df0f7a101e588b1d7b15c1aec592974e0f88e
Sebastian Benad <sebastian@Sebastians-MacBook-Pro.local>
2024-03-25 22:00:51 -0600

    Traverse internal website routes and check for email and phone numbers

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 from bs4 import BeautifulSoup
 import urllib.request
-import re
+from textParsers import extract_emails, extract_phone_numbers
+from elementParsers import extract_website_routes
 
-def extract_emails(input_string):
-    # Regex pattern for matching email addresses
-    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+# Remove trailing slash from the URL for now
+testUrl = ""
 
-    # Find all matches using the findall method from the re module
-    emails_found = re.findall(email_pattern, input_string)
-
-    return emails_found
-
-def extract_phone_numbers(input_string):
-    phone_pattern = r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})'
-    phone_numbers_found = re.findall(phone_pattern, input_string)
-    
-    return phone_numbers_found
-
-
-url = ""
-text = urllib.request.urlopen(url).read()
-
-
-soup = BeautifulSoup(text, 'html.parser')
-emails = extract_emails(soup.get_text())
-phone_numbers = extract_phone_numbers(soup.get_text())
-print(soup.title.string)
-print(emails)
-print(phone_numbers)
+def extract_data_from_url(url):
+    text = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(text, 'html.parser')
+    title = soup.title.strings
+    emails = extract_emails(soup.get_text())
+    phone_numbers = extract_phone_numbers(soup.get_text())
+    all_link_elements = soup.find_all("a", href=True)
+    all_links = extract_website_routes(all_link_elements)
+    for link in all_links:
+        try:
+            new_text = urllib.request.urlopen(url + link).read()
+            new_soup = BeautifulSoup(new_text, 'html.parser')
+            emails += extract_emails(new_soup.get_text())
+            phone_numbers += extract_phone_numbers(new_soup.get_text())
+        except:
+            pass
+    emails = list(set(emails))
+    phone_numbers = list(set(phone_numbers))
+    return {"title": title, "emails": emails, "phone_numbers": phone_numbers}
 
 
+data = extract_data_from_url(testUrl)
+
+print(data)
