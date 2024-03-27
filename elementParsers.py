34940c0d7a05a8bf4dcf297675fe420d835151d9
Sebastian Benad <sebastian@Sebastians-MacBook-Pro.local>
2024-03-27 10:42:26 -0600

    Move logic into lead class

--- elementParsers.py
+++
@@ -1,9 +0,0 @@
-# Takes a list of link elements and returns paths that are internal to the website
-def extract_website_routes(links):
-    cleaned_links = list(filter(lambda x: not x['href'].startswith('http'), links))
-    cleaned_links = list(filter(lambda x: x['href'].startswith('/'), cleaned_links))
-    final_links = []
-    for link in cleaned_links:
-        final_links.append(link['href'])
-    final_links = list(set(final_links))
-    return final_links
